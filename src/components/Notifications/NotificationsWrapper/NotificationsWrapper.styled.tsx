fa6d12686b1230a7129efb1ffd3dbe56a437edf3
dhruvmisra <dhruvmisra@live.com>
2021-04-14 14:00:46 +0530

    Add CSS fixes for scroll bar

--- src/components/Notifications/NotificationsWrapper/NotificationsWrapper.styled.tsx
+++ src/components/Notifications/NotificationsWrapper/NotificationsWrapper.styled.tsx
@@ -4,13 +4,15 @@ import { NotificationsItemStyled } from "../NotificationsItem/NotificationItem.s
 
 
 export const NotificationsWrapperStyled = styled.div`
-    position   : fixed;
-    display    : flex;
-    width      : 400px;
-    z-index    : 5000000;
-    padding    : 4vh 2vw;
-    overflow-y : scroll;
-    overflow-x : hidden;
+    position           : fixed;
+    display            : flex;
+    width              : 400px;
+    z-index            : 5000000;
+    padding            : 4vh 2vw;
+    overflow-y         : auto;
+    overflow-x         : hidden;
+    -ms-overflow-style : none;  /* IE and Edge */
+    scrollbar-width    : none;  /* Firefox */
 
     &.left  { left : 0; }
     &.right { right : 0; }
@@ -32,5 +34,10 @@ export const NotificationsWrapperStyled = styled.div`
     &.bottom {
         ${NotificationsItemStyled}:first-of-type { margin-bottom : 0; }
         ${NotificationsItemStyled}:last-of-type  { margin-top : 0; }
+    }
+
+    /* Hide scrollbar for Chrome, Safari and Opera */
+    &::-webkit-scrollbar {
+        display : none;
     }
 `;
